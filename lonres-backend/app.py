fa8ea7c11f3d621fc79132bdf37f59574c76241b
HO\agoddard <agoddard@kfh.co.uk>
2025-07-17 09:41:44 +0100

    Updated port to make Render work

--- lonres-backend/app.py
+++ lonres-backend/app.py
@@ -32,4 +32,6 @@ def receive_data():
         return jsonify({"error": "Internal server error"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
